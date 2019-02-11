1e64bd3ce3a0d464b4c09a0e0c331890a83315b9
x <x@zol.su>
2019-02-12 00:12:49 +0300

    Коментарии к коду

--- Sync/Race.go
+++ Sync/Race.go
@@ -8,15 +8,16 @@ import (
 	"sync"
 )
 
-func main()  {
-	var wg sync.WaitGroup
-	w := newWord()
-	for _, f := range os.Args[1:]{
+func main() {
+	var wg sync.WaitGroup // снова используется группа ожидания для мониторинга группы сопрограмм
+	w := newWords()
+	for _, f := range os.Args[1:] {
 		wg.Add(1)
 		go func(file string) {
-			if err := tallyWords(file, w); err != nil{
-				fmt.Println(err.Erroor())
+			if err := tallyWords(file, w); err != nil {
+				fmt.Println(err.Error())
 			}
+			wg.Done()
 		}(f)
 	}
 	wg.Wait()
@@ -32,12 +33,12 @@ type words struct {
 	found map[string]int
 }
 
-func newWords() *words{
+func newWords() *words {
 	return &words{found: map[string]int{}}
 }
 
-func (w *words)add(word string, n int){
-	count, ok := w.found(word)
+func (w *words) add(word string, n int) {
+	count, ok := w.found[word]
 	if !ok {
 		w.found[word] = n
 		return
@@ -46,15 +47,15 @@ func (w *words)add(word string, n int){
 }
 
 func tallyWords(filename string, dict *words) error {
-	file, err := os.Open(filename)
-	if err != nil{
+	file, err := os.Open(filename)
+	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	scaner := bufio.NewScaner(file)
+	scaner := bufio.NewScanner(file) //disassemble //scaner - полезный инструмент для подбного анализа файлов
 	scaner.Split(bufio.ScanWords)
-	for scaner.Scan(){
+	for scaner.Scan() {
 		word := strings.ToLower(scaner.Text())
 		dict.add(word, 1)
 	}
