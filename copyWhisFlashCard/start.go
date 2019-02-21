7febcdf912f3ca85ee1123c984b9e3b92838918c
x <x@zol.su>
2019-02-21 12:34:14 +0300

    Монтирование и копирование данных с карты автоматически

--- copyWhisFlashCard/start.go
+++ copyWhisFlashCard/start.go
@@ -9,27 +9,34 @@ import (
 
 //uuid идентификатор диска
 const UUID string = "29ED-2200"
+const DEVDISK = "/dev/disk/by-uuid"
 
 type Card struct {
-	UUID  string          //uuid
-	found map[string]bool //подключенные устройства
+	UUID    string          //uuid
+	found   map[string]bool //подключенные устройства
+	mounted bool            //смонтирован?
 }
 
 type Mounter interface {
-	Mnt() bool
-	isNeedCard(file os.FileInfo) bool
+	Check() bool
+	IsNeedCard(file os.FileInfo) bool
+	IsMount(file os.FileInfo) bool
 }
 
 func main() {
+	var Card Mounter = Card{
+		UUID,
+		map[string]bool{},
+		false}
+	//}
 	for {
-		var Card Mounter = Card{UUID, map[string]bool{}}
-		Card.Mnt()
+		Card.Check()
 		println("Exit.")
 	}
 }
 
-func (c Card) Mnt() bool {
-	files, err := ioutil.ReadDir("/dev/disk/by-uuid")
+func (c Card) Check() bool {
+	files, err := ioutil.ReadDir(DEVDISK)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,19 +44,31 @@ func (c Card) Mnt() bool {
 	for _, file := range files {
 		if value, ok := c.found[file.Name()]; ok {
 			if value {
-				//выполняем копирование
+				if c.IsMount(file) {
+
+				} else {
+
+				}
 			} else {
-				c.isNeedCard(file)
+				c.IsNeedCard(file)
 			}
 		} else {
-			c.isNeedCard(file)
+			c.IsNeedCard(file)
 		}
 		fmt.Println(file.Name())
 	}
 	return true
 }
 
-func (c Card) isNeedCard(file os.FileInfo) bool {
+func (c Card) IsMount(file os.FileInfo) bool {
+	var allLink string = DEVDISK + "/" + c.UUID
+	f, _ := os.Readlink(allLink)
+	println(f)
+
+	return true
+}
+
+func (c Card) IsNeedCard(file os.FileInfo) bool {
 	if file.Name() == c.UUID {
 		c.found[file.Name()] = true
 		return true
